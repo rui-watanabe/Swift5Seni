c42515e328799df6407fd01cc911fa42157fe972
rui-watanabe <wataru610806@gmail.com>
2020-07-04 11:28:12 +0900

    move to modal

---
+++ NextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NextViewController.swift
+//  Swift5seni
+//
+//  Created by watar on 2020/07/04.
+//  Copyright © 2020 rui watanabe. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
